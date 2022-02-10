af8dcecd8cfc0b822d8094b123cdf254864154f4
Bruno Lazzara Queiroz <34097455+bruno-lazzara@users.noreply.github.com>
2022-02-10 01:05:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioMultiplosDe3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero = 1;
+            while (numero <= 100)
+            {
+                if (numero % 3 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+                numero++;
+            }
+
+            Console.WriteLine();
+            // OU...
+
+            for (int numero2 = 1; numero2 <= 100; numero2++)
+            {
+                if (numero2 % 3 == 0)
+                    Console.WriteLine(numero2);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
